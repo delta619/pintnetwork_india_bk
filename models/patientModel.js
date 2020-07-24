d5aaa8031e7895c2cd24c7bfb007c4aa4f993759
delta619 <deltareaper619@gmail.com>
2020-07-24 21:57:45 +0530

    commit

--- models/patientModel.js
+++ models/patientModel.js
@@ -1,62 +1,57 @@
-const mongoose = require("mongoose");
-
+const mongoose = require('mongoose');
 
 const patientSchema = new mongoose.Schema({
-   
-    name:{
-        type:String,
-        trim:true
-    },
-    age:{
-        type:Number,
-        trim:true
-    },
-    sex:{
-        type:String,
-    },
-    contact:{
-        type:String,
-        unique:true
-    },
-    email:{
-        type:String,
-        unique:true
-    },
-    blood:{
-        type:String,
-    },
-    city:{
-        type: String,
-    },
-    hospital:{
-        type:String,
-        default: "Not mentioned"
-    },
-    doctorPrescription:{
-        type:Number,
-    },
-    labDiagnosed:{
-        type:Number
-    },
-    registeredAt: {
-        type: Date, 
-        default: Date.now()
-    },
-    healthy:{
-        type:Boolean,
-        default: true
-    },
-    matchedEarlier:{
-        type:Boolean,
-        default:false
-    },
-    matchedTo: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'Donor'
-    }
+  name: {
+    type: String,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    trim: true,
+  },
+  sex: {
+    type: String,
+  },
+  contact: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  blood: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  hospital: {
+    type: String,
+    default: 'Not mentioned',
+  },
+  doctorPrescription: {
+    type: Number,
+  },
+  labDiagnosed: {
+    type: Number,
+  },
+  registeredAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  healthy: {
+    type: Boolean,
+    default: true,
+  },
+  matchedEarlier: {
+    type: Boolean,
+    default: false,
+  },
+  matchedTo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Donor',
+  },
 });
 
-
-const Patient = mongoose.model('Patient',patientSchema);
+const Patient = mongoose.model('Patient', patientSchema);
 
 module.exports = Patient;
