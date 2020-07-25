36cc67b560f548b51e5327330dea0b3eb42a661d
delta619 <deltareaper619@gmail.com>
2020-07-26 00:09:27 +0530

    name set in matchedTo , pdf removed

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const express = require("express");
-
+const express = require('express');
 
 const app = express();
 const donorRoute = require('./routes/donorRoute');
@@ -8,33 +7,32 @@ const adminRoute = require('./routes/adminRoute');
 
 const Hit = require('./models/hitModel');
 
-const globalErrorController = require("./controller/errorController");
-const morgan = require("morgan");
-const rateLimit = require("express-rate-limit");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
+const globalErrorController = require('./controller/errorController');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const helmet = require('helmet');
-const hpp = require("hpp");
+const hpp = require('hpp');
 const cors = require('cors');
 // GLOBAL
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 // protect
 app.use(helmet());
 
-
 const limiter = rateLimit({
-    max:100,
-    windowMs: 60*60*1000,   // MAX 100 requests in 1 hr
-    message: "Too many requests from your ip, Please try again in an hour"
-})
+  max: 100,
+  windowMs: 60 * 60 * 1000, // MAX 100 requests in 1 hr
+  message: 'Too many requests from your ip, Please try again in an hour',
+});
 
-
-app.use('/api',limiter)
-
+if (process.env.NODE_ENV === 'development') {
+  app.use('/api', limiter);
+}
 
 // BODY PARSER
-app.use(express.json({limit: '10kb'}));
+app.use(express.json({ limit: '10kb' }));
 
 // prevent NOSQL injection
 app.use(mongoSanitize());
@@ -46,62 +44,58 @@ app.use(xss());
 // setting global cors
 
 // if(process.env.NODE_ENV == "development"){
-    app.use(cors());
+app.use(cors());
 // }
 
 // prevent parameter pollution
 
-app.use(hpp({
-    whitelist:['duration','ratingsQuantity','ratingsAverage','maxGroupSize','difficulty','price']
-}));
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
-
-
-app.get('/api/addHit',async(req , res )=>{
-    
-    if(process.env.NODE_ENV == "production"){
-        try {
-            await Hit.create({
-                hit:1,
-                data: JSON.stringify(req.headers),
-            })
-        } catch (error) {
-            console.log(error); 
-        }
-    
+app.get('/api/addHit', async (req, res) => {
+  if (process.env.NODE_ENV == 'production') {
+    try {
+      await Hit.create({
+        hit: 1,
+        data: JSON.stringify(req.headers),
+      });
+    } catch (error) {
+      console.log(error);
     }
-    res.json({
-        status:200
-    })
+  }
+  res.json({
+    status: 200,
+  });
+});
 
-})
+app.use('/api/donor', donorRoute);
+app.use('/api/patient', patietRoute);
+app.use('/api/admin', adminRoute);
 
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //     status:"failed",
+  //     message:`Couldnt find ${req.originalUrl} `
+  // })
+  console.log('unhandled route was traced');
 
-app.use('/api/donor',donorRoute);
-app.use('/api/patient',patietRoute)
-app.use('/api/admin',adminRoute)
+  const err = new App(`Couldnt find ${req.originalUrl}`);
+  err.statusCode = 404;
+  next(err);
+});
 
-
-app.all('*',(req , res , next)=>{
-    // res.status(404).json({
-    //     status:"failed",
-    //     message:`Couldnt find ${req.originalUrl} `
-    // })
-    console.log("unhandled route was traced");
-    
-    const err = new App(`Couldnt find ${req.originalUrl}`)
-    err.statusCode = 404;
-    next(err);
-})
-
-app.use(globalErrorController)
+app.use(globalErrorController);
 
 exports.Server = app;
-
-
-
-
-
-
--- controller/emailController.js
+++ controller/emailController.js
@@ -1,44 +1,38 @@
-const email = require("../utils/email");
-const constants = require('../constants')
+const email = require('../utils/email');
+const constants = require('../constants');
 
-
-
-
-exports.sendMatchMailDonor = async(currentDonor, session_otp) => {
-    try {
-       await email.sendEmailWithAttachments({
-            email: currentDonor.email,
-            subject: 'PintNetwork - Patient Found',
-            message: `<br>Dear ${currentDonor.name},<br>
+exports.sendMatchMailDonor = async (currentDonor, session_otp) => {
+  try {
+    await email.sendEmailPlain({
+      email: currentDonor.email,
+      subject: 'PintNetwork - Patient Found',
+      message: `<br>Dear ${currentDonor.name},<br>
         <br>Great news! We’ve found you a patient who needs plasma.<br>
         <br>Here is the patient OTP: ${session_otp}<br>
         <br>The patient will contact you within the next 24-48 hours. In case of any further delay, please
         <br>reach out to us so we can match you to another patient in need.<br>
-        <br>We’ve attached below a copy of your medical history to ease the process of donation.<br>
         <br>Thank you for believing in us and for your relentless service to humanity.<br>
         <br>You are one step closer to saving a life ☺<br>
         <br>P.S : Do let us know if the match turned out to be successful and if we can assist you in any
         <br>further way<br>
         <br>Regards,
         <br>Team PINT`,
-            attachments: [{
-                filename: `${currentDonor.name} Donor Form.pdf`,
-                path: `${constants.DONOR_FORM_ATTACHMENT_PATH}/${currentDonor._id}.pdf`
-            }],
-        })
-        
-    } catch (e) {
-        throw e;
-    }
-    
-}
+    });
+  } catch (e) {
+    throw e;
+  }
+};
 
-exports.sendMatchMailPatient = async(currentDonor,currentPatient, session_otp) => {
-   try {
+exports.sendMatchMailPatient = async (
+  currentDonor,
+  currentPatient,
+  session_otp
+) => {
+  try {
     email.sendEmailPlain({
-        email: currentPatient.email,
-        subject: 'PintNetwork - Donor Found',
-        message: `Dear ${currentPatient.name},<br>
+      email: currentPatient.email,
+      subject: 'PintNetwork - Donor Found',
+      message: `Dear ${currentPatient.name},<br>
         <br>Great news! We’ve found you a donor.<br>
         <br>Provided below are the contact details of your nearest donor:<br>
         <br>Name:${currentDonor.name}
@@ -51,11 +45,9 @@ exports.sendMatchMailPatient = async(currentDonor,currentPatient, session_otp) =
         <br>P.S: Do let us know if the match turned out to be successful and if you’d be interested in donating as well post-recovery
         <br>Regards,
         <br>Team PINT
-        `
-    })
-    
-   } catch (error) {
-       throw e
-   }
-    
-}
+        `,
+    });
+  } catch (error) {
+    throw e;
+  }
+};
