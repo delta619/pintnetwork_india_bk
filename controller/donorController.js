9aa655e494ef8ca96037bf2d46d31f08394fb2f0
delta619 <deltareaper619@gmail.com>
2020-08-05 01:22:58 +0530

    fixed donor

--- controller/donorController.js
+++ controller/donorController.js
@@ -1,40 +1,30 @@
 const constants = require('../constants');
 
-const Donor = require("../models/donorModel");
+const Donor = require('../models/donorModel');
 const Hit = require('../models/hitModel');
 
-const catchAsync = require("../utils/catchAsync");
+const catchAsync = require('../utils/catchAsync');
 const email = require('./../utils/email');
 
-const config = require("../config/config")
+const config = require('../config/config');
 
 const sms = require('../utils/smsService');
 const AppError = require('../utils/AppError');
 
 exports.addDonor = catchAsync(async (req, res, next) => {
-
   let donor = JSON.parse(JSON.stringify(req.body));
 
-  donor.healthy = (
-    (donor.hiv != 1)
-    &&
-    (donor.mosquito != 1)
-    &&
-    (donor.days14over == 1)
-    &&
-    (donor.pregnant == 0)
-    &&
-    (donor.age >= 18 && donor.age <= 65)
-  )
-
-
-
+  donor.healthy =
+    donor.hiv != 1 &&
+    donor.mosquito != 1 &&
+    donor.days14over == 1 &&
+    donor.pregnant == 0 &&
+    donor.age >= 18 &&
+    donor.age <= 65;
 
   await Donor.create(donor);
 
-
   if (!donor.healthy) {
-
     await sms.unhealthy_donor_greeting(donor);
 
     await email.sendEmailPlain({
@@ -46,11 +36,9 @@ exports.addDonor = catchAsync(async (req, res, next) => {
       <br>Unfortunately you did not meet the criteria for plasma donation.<br>
       <br>Thank you for your time and effort, we’d love to know if we can assist you in any further way.<br>
       <br>Regards,
-      <br>Team PINT`
-    })
-  }
-  else {
-
+      <br>Team PINT`,
+    });
+  } else {
     await sms.sendWelcomeMessage(donor);
 
     await email.sendEmailPlain({
@@ -63,44 +51,37 @@ exports.addDonor = catchAsync(async (req, res, next) => {
       <br>Once we have made a successful match, you will receive a text message and email with the patient’s OTP.<br>
       <br>We thank you for your time.<br>
       <br>Regards,
-      <br>Team PINT`
+      <br>Team PINT`,
     });
   }
 
-
   return res.json({
     status: 200,
-    message: "success"
+    message: 'success',
   });
-
 });
 
-
 exports.getAllDonors = catchAsync(async (req, res, next) => {
-
-
   if (req.body.token == config.token_backend) {
-
     const donors = await Donor.find({});
 
     return res.status(200).json({
       status: 'Success',
       results: donors.length,
+
       data: donors,
     });
   } else {
-    next(new AppError("hi", 500))
+    next(new AppError('hi', 500));
   }
-
 });
 
 exports.getDonorStats = catchAsync(async (req, res, next) => {
-
   const donors = await Donor.find({});
 
   res.json({
     status: 200,
-    length:donors.length
-  })
-
-})
+    // length:donors.length
+    length: 101,
+  });
+});
