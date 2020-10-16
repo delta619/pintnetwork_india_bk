b37b0ba93e3d5f5001d45b123d5d67b129a5a128
Ashutosh Malla <deltareaper619@gmail.com>
2020-10-17 00:57:17 +0530

    error mail for global error

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,8 +1,12 @@
+const { sendErrorMail } = require('./emailController');
+
 module.exports = async (err, req, res, next) => {
   console.log(err);
 
   if (process.env.NODE_ENV == 'production') {
     err.message = 'Something went wrong';
+
+    sendErrorMail(err);
   }
 
   res.json({
